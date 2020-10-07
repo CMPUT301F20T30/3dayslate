3a0bcfe1fecd5b9b22b7131ee5b7732e3bf5cb41
Jensen Khemchandani <jkhemcha@ualberta.ca>
2020-10-07 17:21:48 -0600

    Sign up is complete, not login, and added unique username

--- app/src/main/java/com/jensen/demo/a3dayslate/MainActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/MainActivity.java
@@ -3,6 +3,7 @@ package com.jensen.demo.a3dayslate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -35,8 +37,9 @@ public class MainActivity extends AppCompatActivity {
     // Declare text fields for user information
     private EditText enterEmail;
     private EditText enterPassword;
+    private EditText enterUsername;
 
-    // Variable to keep track of the current user's ID
+    // Variable to keep track of the user's ID for sign-up
     String userID;
 
     @Override
@@ -50,23 +53,12 @@ public class MainActivity extends AppCompatActivity {
         // Connect to the FireBase Auth server
         final FirebaseAuth uAuth = FirebaseAuth.getInstance();
 
-         /* This comment contains a test for database insertion!
-        final CollectionReference collectionReference = db.collection("Test");
-
-        HashMap<String, String> data = new HashMap<>();
-        data.put("attr1", "1");
-        data.put("attr2", "2");
-        collectionReference
-                .document("Test")
-                .set(data);
-        */
-
         // Map buttons and EditText fields to XML
         loginButton = findViewById(R.id.login_button);
         signUpButton = findViewById(R.id.signup_button);
         enterEmail = findViewById(R.id.enter_email);
         enterPassword = findViewById(R.id.enter_password);
-
+        enterUsername = findViewById(R.id.enter_username);
 
         // Login button functionality
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -87,21 +79,57 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Get the sign-up data from EditText fields
                 final String userEmail = enterEmail.getText().toString();
+                final String userUsername = enterUsername.getText().toString();
+                //HashMap<String, Object> user;
                 String userPassword = enterPassword.getText().toString();
-                if(userEmail.length() == 0 || userPassword.length() == 0) {
+                if(userEmail.length() == 0 || userPassword.length() == 0 || userUsername.length() == 0) {
                     return;
                 }
+
                 // Use FireBase Auth to set up and authenticate a new User!
                 uAuth.createUserWithEmailAndPassword(userEmail, userPassword)
                         .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
+                            public void onComplete(@NonNull final Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    success = true;
                                     userID = uAuth.getCurrentUser().getUid();
-                                    DocumentReference documentReference = db.collection("users").document(userID);
+                                    final DocumentReference documentReference = db.collection("users").document(userUsername);
+                                    // This code below, checks the database for the username entered, to ensure that it is unique!
+                                    documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document.exists()) {
+                                                    Log.d("TAG", "Username is taken!");
+                                                }
+                                                else {
+                                                    // Case for if the username is indeed unique! -> Actually add the user to the database!
+                                                    HashMap<String, Object> user = new HashMap<>();
+                                                    user.put("email", userEmail);
+                                                    documentReference
+                                                            .set(user)
+                                                            .addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Log.d("TAG", "Failure when trying to sign up user!");
+                                                                }
+                                                            })
+                                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                @Override
+                                                                public void onSuccess(Void aVoid) {
+                                                                    Log.d("TAG", "User profile is created for " + userID);
+                                                                }
+                                                            });
+                                                }
+                                            }
+                                        }
+                                    });
+
+                                    /*
                                     HashMap<String, Object> user = new HashMap<>();
                                     user.put("email", userEmail);
+
                                     documentReference
                                             .set(user)
                                             .addOnFailureListener(new OnFailureListener() {
@@ -115,16 +143,23 @@ public class MainActivity extends AppCompatActivity {
                                                 public void onSuccess(Void aVoid) {
                                                     Log.d("TAG", "User profile is created for " + userID);
                                                 }
+
+
                                     });
+                                    */
+
                                 }
                                 else {
-                                    // If sign in fails, display a message to the user.
+                                    // If sign in fails, display a message to the user?
                                     Log.e("TAG", "onComplete: Failed=" + task.getException().getMessage());
                                 }
 
                                 // ...
                             }
                         });
+                enterEmail.setText("");
+                enterPassword.setText("");
+                enterUsername.setText("");
             }
         });
 
