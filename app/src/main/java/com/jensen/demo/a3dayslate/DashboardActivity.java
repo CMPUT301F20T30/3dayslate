52687fe0a75926ccf586d070473958d8fa6707c7
EricWeber33 <edweber@ualberta.ca>
2020-10-09 23:44:12 -0600

    added dashboard xml and empty activity

---
+++ app/src/main/java/com/jensen/demo/a3dayslate/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.jensen.demo.a3dayslate;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+
+    }
+}
