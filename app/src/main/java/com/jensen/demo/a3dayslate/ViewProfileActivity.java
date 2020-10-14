263927e0bc7f77c299cd056e3e80b6b8743fbeac
Anita-00 <ferenc@ualberta.ca>
2020-10-14 17:36:43 -0600

    added username and email to user profile

--- app/src/main/java/com/jensen/demo/a3dayslate/ViewProfileActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/ViewProfileActivity.java
@@ -5,6 +5,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +29,17 @@ public class ViewProfileActivity extends AppCompatActivity {
         email = findViewById(R.id.view_profile_email);
         phone = findViewById(R.id.view_profile_phone);
         editProfile = findViewById(R.id.edit_profile_button);
+
+        // gets the current user from the database
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final FirebaseAuth uAuth = FirebaseAuth.getInstance();
+        final FirebaseUser currentUser = uAuth.getCurrentUser();
+
+        // sets the texts according to users firebase information
+        username.setText(currentUser.getDisplayName());
+        email.setText(currentUser.getEmail());
+        // NAME????
+        // PHONE NUMBER??
 
         //on click listener for edit profile button
         editProfile.setOnClickListener(new View.OnClickListener() {
