9323d73797f3fe05e49938895b56e302acb84567
Daniel Zaiter <Danny@Daniels-MBP-133.ed.shawcable.net>
2020-10-24 17:00:09 -0600

    Created Book Search Functionailty

--- app/src/main/java/com/jensen/demo/a3dayslate/BookSearchActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/BookSearchActivity.java
@@ -1,20 +1,32 @@
 package com.jensen.demo.a3dayslate;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class BookSearchActivity extends AppCompatActivity {
 
+    private bookCustomList bookAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,25 +35,66 @@ public class BookSearchActivity extends AppCompatActivity {
         //set view
         setContentView(R.layout.book_search_activity);
 
+        /// database connection
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final FirebaseAuth uAuth = FirebaseAuth.getInstance();
+
         //declare array and adapter
         ArrayAdapter<Book> bookAdapter;
         ArrayList<Book> bookDataList = new ArrayList<>();
+        ArrayList<Book> searchDataList = new ArrayList<>();
 
         //declare all xml elements
         Button viewBook;
         Button requestBook;
         Button searchBook;
         ListView bookList;
+        EditText searchBar;
 
         //find view by ID commands
         viewBook = findViewById(R.id.view_book_button);
         requestBook = findViewById(R.id.request_book_button);
         searchBook = findViewById(R.id.book_search_button);
         bookList = findViewById(R.id.book_search_listview);
+        searchBar = findViewById(R.id.book_search_bar);
 
         //set Book Adapter to custom list view
-        bookAdapter = new bookCustomList(this, bookDataList);
+        bookAdapter = new bookCustomList(this, searchDataList);
         bookList.setAdapter(bookAdapter);
+
+        // query database for all books owned by current user
+        db.collection("books")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // log book in database
+                                Log.w("BOOK:", document.getId() + "=>" + document.getData());
+                                //make new bookObject
+                                Book newBook = new Book((String)document.get("title"),
+                                        (String)document.get("isbn"),
+                                        (ArrayList<String>)document.get("authorList"),
+                                        (String)document.get("owner"));
+
+                                // log new book object
+                                Log.w("BOOK Object:", newBook.getTitle() + " " +newBook.getIsbn() + " " + newBook.getAuthors());
+                                // add book to myBooks
+                                bookDataList.add(newBook);
+
+                            }
+                        } else {
+                            Log.w("BOOK:", "Error getting documents");
+                        }
+                        Log.w("BOOK ARRAYLIST:", bookDataList.toString());
+
+                        //bookDataList.setAdapter(bookAdapter);
+                        Log.w("BOOK ARRAYLIST:", bookDataList.toString());
+
+                    }
+
+                });
 
         //a little test
         /*ArrayList<String> authors = new ArrayList<>();
@@ -54,6 +107,25 @@ public class BookSearchActivity extends AppCompatActivity {
         searchBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                searchDataList.clear();
+                String keyword = searchBar.getText().toString().toLowerCase();
+
+
+                for (int i=0; i < bookDataList.size(); i++){
+
+                    Book book = bookDataList.get(i);
+
+                    String bookInfo = book.getIsbn() + book.getTitle();
+                    for (int j=0; j < book.getAuthors().size(); j++){
+                        bookInfo = bookInfo + book.getAuthors().get(j);
+                    }
+
+                    if (bookInfo.toLowerCase().contains(keyword)){
+                        searchDataList.add(book);
+                    }
+                }
+                bookAdapter.notifyDataSetChanged();
 
             }
         });
