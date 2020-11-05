5898336d5d21131ee3d8aec5b7a879540cb698c6
Jensen Khemchandani <jkhemcha@ualberta.ca>
2020-11-04 18:51:37 -0700

    ask for permissions

--- app/src/main/java/com/jensen/demo/a3dayslate/barcodeScannerActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/barcodeScannerActivity.java
@@ -1,14 +1,21 @@
 package com.jensen.demo.a3dayslate;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.zxing.Result;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
 
@@ -31,9 +38,17 @@ public class  barcodeScannerActivity extends AppCompatActivity implements ZXingS
     int gotBook = 1;
     int badResult = 2;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
     }
