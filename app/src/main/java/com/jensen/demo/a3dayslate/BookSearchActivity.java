d0c9ecc73a65391f539768ffd90496e57f359d92
Daniel Zaiter <Danny@Daniels-MBP-932.ed.shawcable.net>
2020-11-05 12:55:21 -0700

    Fixed Commitment Issues

--- app/src/main/java/com/jensen/demo/a3dayslate/BookSearchActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/BookSearchActivity.java
@@ -37,12 +37,50 @@ import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/* BookSearchActivity
+
+   Version 1.0.0
+
+   October 17 2020
+
+   Copyright [2020] [Danny Zaiter, Eric Weber]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+/**
+ * Book Search Activity allows for the searching of books in order to view their
+ * information or make a request to borrow, the books are filtered through a keyword
+ * search implemented in this Activity
+ */
 public class BookSearchActivity extends AppCompatActivity implements Serializable{
 
     private BookCustomList bookAdapter;
     private Book clickedBook = null;
     private int VIEW_BOOK_ACTIVITY = 0;
 
+    //declare all xml elements
+    Button viewBook;
+    Button requestBook;
+    Button searchBook;
+    ListView bookList;
+    EditText searchBar;
+
+    /**
+     * This method begins the activity
+     * @param savedInstanceState
+     *        Bundle
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,13 +98,6 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
         ArrayList<Book> bookDataList = new ArrayList<>();
         ArrayList<Book> searchDataList = new ArrayList<>();
 
-        //declare all xml elements
-        Button viewBook;
-        Button requestBook;
-        Button searchBook;
-        ListView bookList;
-        EditText searchBar;
-
         //find view by ID commands
         viewBook = findViewById(R.id.view_book_button);
         requestBook = findViewById(R.id.request_book_button);
@@ -82,6 +113,10 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
         db.collection("books")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    /**
+                     * This method pulls all books from the DB into the search
+                     * @param task
+                     */
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
@@ -114,6 +149,11 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
         //end test
 
         searchBook.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method initializes a new search with a keyword specified in the search bar
+             * @param v
+             *        The activity's View
+             */
             @Override
             public void onClick(View v) {
 
@@ -143,6 +183,12 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
 
         //on click listener for view book button
         viewBook.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method opens the ViewBookActivity for a selected book,
+             * or does nothing if no book is selected after a click on the viewBook button
+             * @param v
+             *        The activity's View
+             */
             @Override
             public void onClick(View v) {
                 //view clicked book
@@ -156,6 +202,12 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
 
         //on click listener for request book button
         requestBook.setOnClickListener(new View.OnClickListener() {
+            /**
+             * This method initializes a Request object for a selected book
+             * and modifies the DB accordingly after a click on the requestBook button
+             * @param v
+             *        The activity's View
+             */
             @Override
             public void onClick(View v) {
                 if(clickedBook!=null){
@@ -187,6 +239,17 @@ public class BookSearchActivity extends AppCompatActivity implements Serializabl
 
         //on item click listener for list of books
         bookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            /**
+             * This method facilitates the selection of a Book object via a click on the ListView
+             * @param parent
+             *        The parent AdapterView
+             * @param view
+             *        The activity's View
+             * @param position
+             *        The position of the item
+             * @param id
+             *        The ID of the item
+             */
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //background colour of latest clicked book should be highlighted #C0EFE5
--- app/src/main/java/com/jensen/demo/a3dayslate/ViewBookActivity.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/ViewBookActivity.java
@@ -41,6 +41,12 @@ public class ViewBookActivity extends AppCompatActivity implements Serializable 
     private Book book;
     private String authorString;
 
+    //declare xml attributes
+    TextView viewTitle;
+    TextView viewAuthor;
+    TextView isbn;
+    TextView owner;
+
     /** sets up activity
      *
      * @param savedInstanceState
@@ -50,11 +56,7 @@ public class ViewBookActivity extends AppCompatActivity implements Serializable 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_book_activity);
-        //declare xml attributes
-        TextView viewTitle;
-        TextView viewAuthor;
-        TextView isbn;
-        TextView owner;
+
 
         viewTitle = findViewById(R.id.view_book_title);
         viewAuthor = findViewById(R.id.view_book_author);
