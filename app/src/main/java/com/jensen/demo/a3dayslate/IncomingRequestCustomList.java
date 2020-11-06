a4334c7700c59ab1332772d3e8d289a906a3861e
Jensen Khemchandani <jkhemcha@ualberta.ca>
2020-11-05 19:29:37 -0700

    Added even more javadocs

--- app/src/main/java/com/jensen/demo/a3dayslate/IncomingRequestCustomList.java
+++ app/src/main/java/com/jensen/demo/a3dayslate/IncomingRequestCustomList.java
@@ -14,17 +14,63 @@ import androidx.annotation.RequiresApi;
 
 import java.util.ArrayList;
 
+/* IncomingRequestCustomList class
+
+   Version 1.0.0
+
+   October 17 2020
+
+   Copyright [2020] [Jensen Khemchandani]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+/**
+ * This class allows us to make a custom display for each Request object in our IncomingRequestsActivity
+ * Also attaches the listview to the ArrayList of Requests
+ */
+
 public class IncomingRequestCustomList extends ArrayAdapter<Request> {
     private ArrayList<Request> requests ;
     private Context context;
 
 
     // Constructor
+
+    /**
+     * Constructor
+     * @param context
+     * The current context of the app
+     * @param requests
+     * The ArrayList of requests to attach the adapter to
+     */
     public IncomingRequestCustomList(Context context, ArrayList<Request> requests){
         super(context,0, requests);
         this.requests = requests;
         this.context = context;
     }
+
+    /**
+     * Gets and sets up the view for the adapter
+     * @param position
+     * The position in the ArrayList
+     * @param convertView
+     * The view to be converted
+     * @param parent
+     * The parent ViewGroup
+     * @return
+     * Returns a view for the adapter
+     */
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @NonNull
